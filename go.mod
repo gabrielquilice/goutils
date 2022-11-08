e7624aab905b71dc386d73e9562701296d611b28
Gabriel Quilice <gabriel.quilice@aswti.com.br>
2022-11-08 11:03:39 -0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gabrielquilice/goutils
+
+go 1.19
---
+++ utils/utils.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"log"
+	"reflect"
+)
+
+// All valid types
+type AnyValue interface {
+	any | *any
+}
+
+// Simulates the behavior of a ternary if
+func TernaryIf[T AnyValue](condition bool, trueValue, falseValue T) T {
+	if condition {
+		return trueValue
+	} else {
+		return falseValue
+	}
+}
+
+// Checks whether the variable was instantiated or not
+func IsZero[T AnyValue](v T) bool {
+	return reflect.ValueOf(&v).Elem().IsZero()
+}
+
+// Returns the first value if it's not null, otherwise it returns the second
+func GetValueOrElse[T AnyValue](value, replacement T) T {
+	if IsZero(value) {
+		return replacement
+	} else {
+		return value
+	}
+}
+
+// Display formatted string in console
+func PrintString(s string, a ...any) {
+	log.Printf(s+"\n", a)
+}
